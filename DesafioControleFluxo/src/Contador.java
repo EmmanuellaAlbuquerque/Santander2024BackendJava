5a0a2f588cf566f812ce2fd03ffe35eb837db794
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-06-21 18:41:38 -0300

    add DesafioControleFluxo project

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,36 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        try {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+    
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
+
+            contar(parametroUm, parametroDois);
+        } catch (InputMismatchException e) {
+            System.err.println("Os parâmetros devem ser numéricos ");
+        }
+        catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+		
+        terminal.close();
+	}
+
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+		
+		int contagem = parametroDois - parametroUm;
+		for (int i = 0; i < contagem; i++) {
+            System.out.println("Imprimindo o número " + (i + 1));
+        }
+	}
+}
